fe0319de604aa558d1d00d61f4234d28cbdc9bc9
Quadratech188 <quadratech188@gmail.com>
2026-01-05 01:23:46 +0900

    feat: Simplify message handling, define add_rule

--- extension/background-listener.js
+++ extension/background-listener.js
@@ -1,29 +1,44 @@
-// @ts-check
-
-// Firefox - we already injected everything with the 'scripts' key
-if (typeof importScripts === 'undefined') {
-
-}
-// Chromium
-else {
-	importScripts('browser-polyfill.min.js')
-	importScripts('util.js')
-	importScripts('move-download.js')
-}
-
-browser.runtime.onConnect.addListener((port) => {
-	port.onMessage.addListener(async (/** @type RequestedDownload */message) => {
-		let id
-		switch (message.meta.reproduce.type) {
-			case 'tab':
-				id = await tab_download(message.meta.reproduce.args)
-				break
-			case 'browser':
-				id = await browser_download(message.meta.reproduce.args)
-				break
-		}
-		const storage = {}
-		storage[`file_attrs:${id}`] = message
-		browser.storage.local.set(storage)
-	})
+browser.runtime.onMessage.addListener(message => {
+	switch(message.type) {
+		case 'send_download':
+			handle_send_download(message.body)
+			break
+		case 'add_rule':
+			handle_add_rule(message.body)
+			break
+	}
 })
+
+/**
+ * @param {RequestedDownload} request
+ */
+async function handle_send_download(request) {
+	let id
+	switch (request.meta.reproduce.type) {
+		case 'tab':
+			id = await tab_download(request.meta.reproduce.args)
+			break
+		case 'browser':
+			id = await browser_download(request.meta.reproduce.args)
+			break
+	}
+	const storage = {}
+	storage[`file_attrs:${id}`] = request
+	browser.storage.local.set(storage)
+}
+
+/**
+ * @param {SerializedRule} rule 
+ */
+async function handle_add_rule(rule) {
+	/** @type SerializedRule[] */
+	const rules = (await browser.storage.local.get({
+		rules: []
+	}))['rules']
+
+	rules.push(rule);
+
+	await browser.storage.local.set({
+		rules: rules
+	})
+}
--- extension/extractor-interface.js
+++ extension/extractor-interface.js
@@ -17,18 +17,25 @@
  * @param {Record<string, string>} attrs 
  */
 function send_download(download_config, attrs) {
-	const port = browser.runtime.connect({
-		name: 'download_request'
-	})
-	port.onMessage.addListener(() => {
-		// TODO
-	})
 	/** @type RequestedDownload */
-	const message = {
+	const request = {
 		attrs,
 		meta: {
 			reproduce: download_config
 		}
 	}
-	port.postMessage(message)
+	browser.runtime.sendMessage({
+		type: 'add_rule',
+		body: request
+	})
+}
+
+/**
+ * @param {Rule} rule 
+ */
+function add_rule(rule) {
+	browser.runtime.sendMessage({
+		type: 'add_rule',
+		body: rule
+	})
 }
